7e810cf02dd90318ded8f2b0ae2da56afcee2694
Kushal Ghimire <kushalghimirecsit081@chitwancollege.edu.np>
2025-05-22 12:42:18 +0545

    python

---
+++ eg2greater.py
@@ -0,0 +1,9 @@
+num1=int(input("enter a number"))
+num2=int(input("enter a number"))
+num3=int(input("enter a number"))
+if (num1>num2 and num1>num3):
+    print("greatest",num1)
+elif(num2>num3 and num2>num3):
+    print("greatest",num2)
+elif (num3>num2 and num3>num1):
+    print("greatest",num3)
