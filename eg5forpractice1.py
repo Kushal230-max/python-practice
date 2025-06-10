1830615950b7bff288491f05f662df939211a218
Kushal Ghimire <kushalghimirecsit081@chitwancollege.edu.np>
2025-06-10 22:37:27 +0545

    kushal

---
+++ eg5forpractice1.py
@@ -0,0 +1,6 @@
+#wap to find the sum of first n number(using for)
+n=7
+sum=0
+for el in range(1,n+1):
+    sum+=el
+print(sum)
---
+++ eg5forpractice2.py
@@ -0,0 +1,6 @@
+#find the factorial of first n numbers
+n=6
+fact=1
+for el in range(1,n+1):
+    fact*=el
+print(fact)
